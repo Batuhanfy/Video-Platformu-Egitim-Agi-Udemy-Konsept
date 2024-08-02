5e51fde90e679807b5cd62ce2c6b88062d81e307
Batuhanfy <137509436+Batuhanfy@users.noreply.github.com>
2024-08-02 22:24:48 +0300

    güncellendi

---
+++ wwwroot/js/home.js
@@ -0,0 +1,47 @@
+﻿let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = index;
+    }
+    document.querySelector('.slides').style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function showNextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function showPrevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+
+
+
+
+let currentIndex2= 0;
+
+function showSlide2(index) {
+    const slides2 = document.querySelectorAll('.slide2');
+    if (index >= slides2.length) {
+        currentIndex2 = 0;
+    } else if (index < 0) {
+        currentIndex2 = slides2.length - 1;
+    } else {
+        currentIndex2 = index;
+    }
+    document.querySelector('.slides2').style.transform = `translateX(-${currentIndex2 * 100}%)`;
+}
+
+function showNextSlide2() {
+    showSlide2(currentIndex2 + 1);
+}
+
+function showPrevSlide2() {
+    showSlide2(currentIndex2 - 1);
+}
