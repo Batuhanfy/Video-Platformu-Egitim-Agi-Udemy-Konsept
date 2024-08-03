3e7d0087befb2ee0c15279e81b1cd0afbb9fc077
Batuhanfy <137509436+Batuhanfy@users.noreply.github.com>
2024-08-04 01:05:26 +0300

    Güncellendi

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using UdemyEgitimPlatformu.Data;
+using UdemyEgitimPlatformu.Models;
 
 namespace BidemyLearning
 {
@@ -10,50 +12,45 @@ namespace BidemyLearning
         {
             var builder = WebApplication.CreateBuilder(args);
 
-
-
-
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? throw new InvalidOperationException("Database Not Connection");
+                ?? throw new InvalidOperationException("Database Not Connection");
+
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+            // Identity servislerini ekleyin
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
-
-
-
-
+            builder.Services.AddRazorPages();
 
             var app = builder.Build();
-
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-
-
-
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication(); // Kimlik doðrulama middleware'ini ekleyin
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            app.MapRazorPages(); // Razor Pages'ý ekleyin
+
             app.Run();
         }
-     
     }
 }
