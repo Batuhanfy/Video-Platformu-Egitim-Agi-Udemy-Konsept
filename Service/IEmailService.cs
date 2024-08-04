292c6fe561c70c7a6885b257240525f8e8468cce
Batuhanfy <137509436+Batuhanfy@users.noreply.github.com>
2024-08-05 02:19:56 +0300

    Create IEmailService.cs

---
+++ Service/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace UdemyEgitimPlatformu.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string body);
+    }
+}
